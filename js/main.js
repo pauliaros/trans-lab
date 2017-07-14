066eecef29ef162ae246bbaf0f6302ea182e8219
Paulina Aros <aros.pau@gmail.com>
2017-07-13 22:12:34 -0400

    intentando validación home

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 /* Holiii acá va tu código también */
+$(document).ready(function(){
+	$(".boton-inicio").click(function(){
+		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#input-email").val())) ){
+   			$("#input-correo").parent().append("<span>Ingrese correo valido</span>");
+  		}
+
+  		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#input-email").val())) ){
+   			$("#input-email").parent().append("<span>Ingrese correo valido</span>");
+  		}
+
+  		if(($("#input-pass").val() == "pass") || ($("#input-pass").val() == 123456) || ($("#input-pass").val() == 098765) || (($("#input-pass").val()).length < 5)){
+  			$("#input-pass").parent().append("<span>Ingrese contraseña valida</span>");
+  		}
+
+  	});
+});
